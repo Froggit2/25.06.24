3bca8a714c48e89910d211ca2db6107153cf633f
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-06-25 10:42:10 +0300

    Add files via upload

---
+++ 1_10.py
@@ -0,0 +1,24 @@
+import time
+from threading import Thread
+from time import sleep
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+def numb():
+    for num in range(10):
+        num += 1
+        print(num)
+        time.sleep(1)
+
+
+def lett():
+    for let in letters:
+        print(let)
+        time.sleep(1)
+
+
+number = Thread(target=numb)
+letter = Thread(target=lett)
+
+number.start()
+letter.start()
